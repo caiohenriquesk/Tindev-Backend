71520381da00fa2ae882775216029bc9e350267e
caiohenriquesk <caiohenriquesk@gmail.com>
2019-08-19 22:17:56 -0300

    Testandoo socket.io

--- src/server.js
+++ src/server.js
@@ -4,14 +4,30 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('Nova conexão', socket.id);
+
+  socket.on('hello', message => {
+    console.log(message)
+  })
+
+  setTimeout(() => {
+    socket.emit('world', {
+      message: 'OmniStack'
+    });
+  }, 5000)
+});
 
 mongoose.connect('mongodb+srv://chenrique:caio123456@cluster0-gpja5.mongodb.net/omnistack8?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
